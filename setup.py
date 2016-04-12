a7946f81bc67f96bdbbe708927615673450750da
Marcin Nowak <marcin.j.nowak@gmail.com>
2016-04-12 19:06:27 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-sqltemplate',
-      version='0.4.1',
+      version='0.4.2',
       description='Database querying tool for Django, based on SQL templates',
       classifiers=[
         "Development Status :: 3 - Alpha",
