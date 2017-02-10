b55775d56841af1f44365cfa7ca4f72cdd9bcef2
Marcin Nowak <marcin.j.nowak@gmail.com>
2017-02-10 10:40:05 +0100

    change version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sqltemplate',
-      version='0.5.0-a1',
+      version='0.5.0',
       description='Database querying tool for Django, based on SQL templates',
       classifiers=[
         "Development Status :: 3 - Alpha",
