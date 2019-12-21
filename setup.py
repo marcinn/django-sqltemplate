244577d1497e5f789afe1efb20a90d65fb59e670
Marcin Nowak <marcin.j.nowak@gmail.com>
2019-12-21 23:46:58 +0100

    add makefile, add long_description to setup.py/pypi, update requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
+import os
 from setuptools import setup, find_packages
 
+
+with open(
+    os.path.join(os.path.dirname(__file__), 'README.md'),
+    encoding='utf-8'
+) as fh:
+    long_description = fh.read()
+
+
 setup(name='django-sqltemplate',
-      version='0.5.2',
+      version='0.5.2.post2',
       description='Database querying tool for Django, based on SQL templates',
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Intended Audience :: Developers",
         ],
@@ -23,6 +39,8 @@ setup(name='django-sqltemplate',
       keywords='web python django sql template',
       packages=find_packages('.'),
       include_package_data=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       test_suite='nose.collector',
       zip_safe=True,
       )
